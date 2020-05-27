f9f7870b4ca6ad4bbe459955ba627c42039c5591
Vyctor <dev.vyctor@gmail.com>
2020-05-26 21:38:55 -0300

    Removendo e salvando no localstorage

--- projeto-js-2/src/main.js
+++ projeto-js-2/src/main.js
@@ -3,7 +3,7 @@ import api from './api';
 class App {
   constructor() {
     // Lista de repositórios
-    this.repositories = [];
+    this.repositories = JSON.parse(localStorage.getItem('@repositories')) || [];
 
     // Form
     this.form = document.querySelector("form");
@@ -13,10 +13,7 @@ class App {
 
     // Método para registrar os eventos do form
     this.registerRepository();
-  }
-
-  registerRepository() {
-    this.form.onsubmit = (event) => this.addRepository(event);
+    this.renderScreen();
   }
 
   async addRepository(event) {
@@ -53,6 +50,9 @@ class App {
       });
 
       this.renderScreen();
+
+      this.saveDataToStorage();
+
     } catch (err) {
       this.list.removeChild(document.querySelector('.list-group-item-warning'));
 
@@ -78,31 +78,33 @@ class App {
   }
 
   renderScreen() {
+
     this.list.innerHTML = "";
 
     this.repositories.forEach((repository) => {
       // criando <li>
-      let li = document.createElement("li");
-      li.setAttribute("class", "list-group-item list-group-item-action");
+      let itemList = document.createElement("li");
+      itemList.setAttribute('class', 'list-group-item list-group-item-action');
+      itemList.setAttribute('onclick', 'deleteRepository(this)');
+
+      console.log(itemList)
 
       // criando <img>
       let img = document.createElement("img");
       img.setAttribute("src", repository.avatar_url);
-      li.appendChild(img);
-
-
+      itemList.appendChild(img);
 
       // criando <strong>
       let strong = document.createElement("strong");
       let txtName = document.createTextNode(repository.name);
       strong.appendChild(txtName);
-      li.appendChild(strong);
+      itemList.appendChild(strong);
 
       // criando <p>
       let p = document.createElement("p");
       let txtDescription = document.createTextNode(repository.description);
       p.appendChild(txtDescription);
-      li.appendChild(p);
+      itemList.appendChild(p);
 
       // criando <a></a>
       let a = document.createElement("a");
@@ -110,10 +112,10 @@ class App {
       a.setAttribute("href", repository.link);
       let textAnchor = document.createTextNode("Acessar");
       a.appendChild(textAnchor);
-      li.appendChild(a);
+      itemList.appendChild(a);
 
       // adicionar <li> como filho da ul
-      this.list.appendChild(li);
+      this.list.appendChild(itemList);
 
       // limpar o conteúdo do input
       this.form.querySelector("input[id=repositorio]").value = '';
@@ -121,6 +123,23 @@ class App {
       // adiciona foco no inpuit
       this.form.querySelector("input[id=repositorio]").focus();
     });
+  }
+
+  saveDataToStorage() {
+    localStorage.setItem('@repositories', JSON.stringify(this.repositories))
+  }
+
+  registerRepository() {
+    this.form.onsubmit = (event) => {
+      console.log(event);
+      this.addRepository(event);
+    };
+  }
+
+  deleteRepository(repository) {
+    this.repositories.splice(this.repositories.indexOf(repository.name), 1);
+    this.saveDataToStorage();
+    this.renderScreen();
   }
 }
 
